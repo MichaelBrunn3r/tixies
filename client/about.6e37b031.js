754fe4ce2ee26f92da80cee0bb05818f0d0015c0
MichaelBrunn3r <MichaelBrunn3r@gmail.com>
2020-12-12 14:57:39 +0100

    Updates

--- client/about.6e37b031.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as s,s as e,y as a,d as n,n as o}from"./client.1a26fd60.js";function i(t){return{c(){this.h()},l(t){a('[data-svelte="svelte-1ine71f"]',document.head).forEach(n),this.h()},h(){document.title="About"},m:o,p:o,i:o,o:o,d:o}}export default class extends t{constructor(t){super(),s(this,t,null,i,e,{})}}
