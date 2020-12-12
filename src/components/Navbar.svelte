08f679b9a5a6dac92002977e958c88d914f47a1b
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-12 18:14:59 +0100

    Added basic navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,20 @@
+<style>
+	.container {
+		display: grid;
+		grid-auto-flow: column;
+		justify-content: center;
+		margin-bottom: 8px;
+		column-gap: 8px;
+	}
+
+	a {
+		color: gray;
+		background: transparent;
+		border: transparent;
+	}
+</style>
+
+<div class="container">
+	<a href='/'>HOME</a>
+	<a href='gallery'>GALLERY</a>
+</div>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,8 @@
+<script>
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
 <main>
+	<Navbar />
 	<slot></slot>
 </main>
