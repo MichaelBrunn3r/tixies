16bb4f7058aefca06b70838c292e4007bc18cb23
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-14 13:46:01 +0100

    Added shader rendering static dots

--- src/components/CanvasTixy.svelte
+++ src/components/CanvasTixy.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { onMount, createEventDispatcher } from 'svelte';
-	import { drawCircle } from '../utils/canvas';
-	import { constrain } from '../utils/math';
 
 	export let time: number;
 	export let speed: number;
@@ -9,18 +7,14 @@
 
 	export let code: string;
 	export let n: number;
-	export let resolution = 1800;
-
-	$: radius = resolution / n / 2;
-	$: diameter = radius*2;
+	export let resolution = 800;
 
 	let dispatch = createEventDispatcher();
 
-	let color1 = '#FF0000';
-	let color2 = '#FFFFFF';
-
 	let canvas: HTMLCanvasElement;
-	let ctx: CanvasRenderingContext2D;
+	let gl: WebGL2RenderingContext;
+	let program: WebGLProgram;
+	let vertices: Float32Array;
 
 	let transform;
 	$: {
@@ -40,35 +34,77 @@
 	}
 
 	// Redraw if n or code changed
-	$: if(ctx && code) drawShapes(1,n);
+	$: if(program && n) updateParams();
 
 	// Redraw if adjustedTime changes
-	$: {
-		if(ctx && speed != 0) {
-			drawShapes(adjustedTime, n);
-		}
+	$: if(program && speed != 0 && adjustedTime) {
+		draw();
+	}
+
+	function createProgram() {
+		gl = canvas.getContext("webgl2");
+
+		gl.clearColor(0, 0, 0, 0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+
+		// Create vertex shader
+		let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+		gl.shaderSource(vertexShader, document.getElementById('vertex-shader').textContent);
+		gl.compileShader(vertexShader);
+
+		// Create Fragment shader
+		let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+		gl.shaderSource(fragmentShader, document.getElementById('fragment-shader').textContent);
+		gl.compileShader(fragmentShader);
+
+		// Create program
+		program = gl.createProgram();
+		gl.attachShader(program, vertexShader);
+		gl.attachShader(program, fragmentShader);
+		gl.linkProgram(program);
+
+		// Create vertex buffer
+		vertices = new Float32Array([
+			-1.0, -1.0,
+			1.0, -1.0,
+			-1.0,  1.0,
+			-1.0,  1.0,
+			1.0, -1.0,
+			1.0,  1.0
+		])
+
+		let buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+		gl.useProgram(program);
+
+		// Create vertex shader parameters
+		program.position = gl.getUniformLocation(program, 'position');
+		gl.enableVertexAttribArray(program.position);
+		gl.vertexAttribPointer(program.position, 2, gl.FLOAT, false, 0, 0);
+
+		// Create fragment shader parameters
+		program.color = gl.getUniformLocation(program, 'color');
+		program.resolution = gl.getUniformLocation(program, 'resolution');
+		gl.uniform3fv(program.resolution, [resolution, resolution, 0]);
+		program.n = gl.getUniformLocation(program, 'n');
+	}
+
+	function updateParams() {
+		gl.uniform4fv(program.color, [0,1,0,1]);
+		gl.uniform1i(program.n, n);
+	}
+
+	function draw() {
+		gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
 	}
 
 	onMount(async () => {
-		ctx = canvas.getContext("2d");
-		ctx.imageSmoothingEnabled = true;
+		createProgram();
+		updateParams();
+		draw();
 	})
-
-	function clear() {
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-	}
-
-	function drawShapes(time: number, n: number) {
-		clear();
-
-		for(let y=0; y<n; y++) {
-			for(let x=0; x<n; x++) {
-				const scale = constrain(transform(time, y*n+x, x, y, n), -1, 1);
-				const color = scale < 0 ? color1 : color2;
-				drawCircle(ctx,radius + x*diameter,radius + y*diameter, radius*Math.abs(scale), color);
-			}
-		}
-	}
 </script>
 
 <style lang="scss">
